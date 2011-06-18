e4b72239e215dc30d6ef5c9f18d119520adafb7d
richard.hyndman@gmail.com <richard.hyndman@gmail.com@31a94f91-a260-7e09-7896-cf7e99866384>
2011-06-18 07:44:50 +0000

    added animation on switch mode
    git-svn-id: http://android-ultimatestopwatch.googlecode.com/svn/trunk@20 31a94f91-a260-7e09-7896-cf7e99866384

--- stopwatch/src/com/geekyouup/android/ustopwatch/fragments/UltimateStopwatchFragments.java
+++ stopwatch/src/com/geekyouup/android/ustopwatch/fragments/UltimateStopwatchFragments.java
@@ -2,6 +2,9 @@ package com.geekyouup.android.ustopwatch.fragments;
 
 import java.util.HashMap;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -29,7 +32,8 @@ import com.geekyouup.android.ustopwatch.fragments.StopwatchFragment;
 import com.geekyouup.android.ustopwatch.fragments.TimeFragment;
 import com.geekyouup.android.ustopwatch.fragments.TimeUtils;
 
-public class UltimateStopwatchFragments extends Activity implements LapTimeRecorder {
+public class UltimateStopwatchFragments extends Activity implements
+		LapTimeRecorder {
 
 	private PowerManager mPowerMan;
 	private PowerManager.WakeLock mWakeLock;
@@ -40,51 +44,57 @@ public class UltimateStopwatchFragments extends Activity implements LapTimeRecor
 	public static final String MSG_REQUEST_COUNTDOWN_DLG = "msg_usw_counter";
 	public static final String MSG_UPDATE_COUNTER_TIME = "msg_update_counter";
 	public static final String MSG_NEW_TIME_DOUBLE = "msg_new_time_double";
-	private static final String WAKE_LOCK_KEY="ustopwatch";
+	private static final String WAKE_LOCK_KEY = "ustopwatch";
 
 	private TimeFragment mCounterView;
 	private StopwatchFragment mStopwatchFragment;
-	private LapTimesFragment mLapTimesFragment; 
+	private LapTimesFragment mLapTimesFragment;
 	private double mCurrentTimeMillis = 0;
-	
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.main);
-		
+
 		mPowerMan = (PowerManager) getSystemService(Context.POWER_SERVICE);
-	    setVolumeControlStream(AudioManager.STREAM_MUSIC);
-		
-		//stop landscape more on QVGA/HVGA
-		int screenSize = getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
-		if(screenSize == Configuration.SCREENLAYOUT_SIZE_SMALL)
-		{
+		setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+		// stop landscape more on QVGA/HVGA
+		int screenSize = getResources().getConfiguration().screenLayout
+				& Configuration.SCREENLAYOUT_SIZE_MASK;
+		if (screenSize == Configuration.SCREENLAYOUT_SIZE_SMALL) {
 			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		}
-		
+
 		// not in all views
-		mCounterView = (TimeFragment) getFragmentManager().findFragmentById(R.id.fragment_time);
+		mCounterView = (TimeFragment) getFragmentManager().findFragmentById(
+				R.id.fragment_time);
 		mCounterView.setLapTimeRecorder(this);
 
-		mStopwatchFragment = (StopwatchFragment) getFragmentManager().findFragmentById(R.id.stopwatch_fragment);
+		mStopwatchFragment = (StopwatchFragment) getFragmentManager()
+				.findFragmentById(R.id.stopwatch_fragment);
 		mStopwatchFragment.setApplication(this);
 		mStopwatchFragment.setHandler(new Handler() {
 			@Override
 			public void handleMessage(Message m) {
 				if (m.getData().getBoolean(MSG_REQUEST_COUNTDOWN_DLG, false)) {
 					requestTimeDialog();
-				} else if (mCounterView != null && m.getData().getBoolean(MSG_UPDATE_COUNTER_TIME, false)) {
-					mCurrentTimeMillis = m.getData().getDouble(MSG_NEW_TIME_DOUBLE);
+				} else if (mCounterView != null
+						&& m.getData().getBoolean(MSG_UPDATE_COUNTER_TIME,
+								false)) {
+					mCurrentTimeMillis = m.getData().getDouble(
+							MSG_NEW_TIME_DOUBLE);
 					if (mCounterView != null)
 						mCounterView.setTime(mCurrentTimeMillis);
 				}
 			}
 		});
-		
-		mLapTimesFragment = (LapTimesFragment) getFragmentManager().findFragmentById(R.id.laptimes_fragment);
-		
+
+		mLapTimesFragment = (LapTimesFragment) getFragmentManager()
+				.findFragmentById(R.id.laptimes_fragment);
+
 		soundPool = new SoundPool(3, AudioManager.STREAM_NOTIFICATION, 100);
 		soundPoolMap = new HashMap<Integer, Integer>();
 		soundPoolMap.put(SOUND_ALARM, soundPool.load(this, R.raw.alarm, 1));
@@ -103,10 +113,11 @@ public class UltimateStopwatchFragments extends Activity implements LapTimeRecor
 		// cancel next alarm if there is one, and clear notification bar
 		AlarmUpdater.cancelCountdownAlarm(this);
 
-		mWakeLock = mPowerMan.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, WAKE_LOCK_KEY);
+		mWakeLock = mPowerMan.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK,
+				WAKE_LOCK_KEY);
 		mWakeLock.acquire();
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
@@ -121,19 +132,36 @@ public class UltimateStopwatchFragments extends Activity implements LapTimeRecor
 		if (item.getItemId() == R.id.menu_playpause) {
 			mStopwatchFragment.startStop();
 		} else if (item.getItemId() == R.id.menu_switchmode) {
-			int newMode = (mStopwatchFragment.getMode()==StopwatchFragment.MODE_STOPWATCH)?StopwatchFragment.MODE_COUNTDOWN:StopwatchFragment.MODE_STOPWATCH;
+			final int newMode = (mStopwatchFragment.getMode() == StopwatchFragment.MODE_STOPWATCH) ? StopwatchFragment.MODE_COUNTDOWN
+					: StopwatchFragment.MODE_STOPWATCH;
 			mStopwatchFragment.setMode(newMode);
-			mLapTimesFragment.setMode(newMode);
 
-			if (newMode==StopwatchFragment.MODE_COUNTDOWN)
-			{
-				requestTimeDialog();
-			}
-
+			ObjectAnimator oa = ObjectAnimator.ofFloat(mLapTimesFragment.getView(), "rotationY", 0,90);
+		    oa.setDuration(250);
+		    oa.addListener(new AnimatorListenerAdapter() {
+		    	@Override
+		    	public void onAnimationEnd(Animator animation) {
+		    		super.onAnimationEnd(animation);
+		    		mLapTimesFragment.setMode(newMode);
+		    		ObjectAnimator oa2 = ObjectAnimator.ofFloat(mLapTimesFragment.getView(), "rotationY", -90,0)
+		    			.setDuration(250);
+		    		
+		    		oa2.addListener(new AnimatorListenerAdapter() {
+		    			@Override
+		    			public void onAnimationEnd(Animator animation) {
+		    				super.onAnimationEnd(animation);
+		    				if (newMode == StopwatchFragment.MODE_COUNTDOWN) {
+		    					requestTimeDialog();
+		    				}
+		    			}
+		    		});
+		    		oa2.start();
+		    	}
+		    });
+			oa.start();
 		} else if (item.getItemId() == R.id.menu_reset) {
 			reset();
-		}else if (item.getItemId()== R.id.menu_laptime)
-		{
+		} else if (item.getItemId() == R.id.menu_laptime) {
 			recordTime();
 		}
 
@@ -143,15 +171,16 @@ public class UltimateStopwatchFragments extends Activity implements LapTimeRecor
 	private void reset() {
 		mStopwatchFragment.reset();
 		mLapTimesFragment.reset();
-		
+
 		if (mCounterView != null)
 			mCounterView.resetTime();
-		
-		if (mStopwatchFragment.getMode()==StopwatchFragment.MODE_COUNTDOWN)
+
+		if (mStopwatchFragment.getMode() == StopwatchFragment.MODE_COUNTDOWN)
 			requestTimeDialog();
 	}
 
 	private boolean mDialogOnScreen = false;
+
 	public void requestTimeDialog() {
 		// stop stacking of dialogs
 		if (mDialogOnScreen)
@@ -163,17 +192,20 @@ public class UltimateStopwatchFragments extends Activity implements LapTimeRecor
 		AlertDialog mSelectTime = new AlertDialog.Builder(this).create();
 		mSelectTime.setView(ll);
 		mSelectTime.setTitle(getString(R.string.timer_title));
-		mSelectTime.setButton(getString(R.string.timer_start), new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) {
-				mDialogOnScreen = false;
-				mStopwatchFragment.setTime(TimeUtils.getDlgHours(), TimeUtils.getDlgMins(), TimeUtils.getDlgSecs());
-			}
-		});
-		mSelectTime.setButton2(getString(R.string.timer_cancel), new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) {
-				mDialogOnScreen = false;
-			}
-		});
+		mSelectTime.setButton(getString(R.string.timer_start),
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						mDialogOnScreen = false;
+						mStopwatchFragment.setTime(TimeUtils.getDlgHours(),
+								TimeUtils.getDlgMins(), TimeUtils.getDlgSecs());
+					}
+				});
+		mSelectTime.setButton2(getString(R.string.timer_cancel),
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						mDialogOnScreen = false;
+					}
+				});
 		mSelectTime.show();
 
 		mDialogOnScreen = true;
@@ -181,8 +213,10 @@ public class UltimateStopwatchFragments extends Activity implements LapTimeRecor
 
 	public void playAlarm() {
 		AudioManager mgr = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-		float streamVolume = mgr.getStreamVolume(AudioManager.STREAM_NOTIFICATION);
-		soundPool.play(soundPoolMap.get(SOUND_ALARM), streamVolume, streamVolume, 1, 0, 1f);
+		float streamVolume = mgr
+				.getStreamVolume(AudioManager.STREAM_NOTIFICATION);
+		soundPool.play(soundPoolMap.get(SOUND_ALARM), streamVolume,
+				streamVolume, 1, 0, 1f);
 	}
 
 	public void notifyCountdownComplete() {

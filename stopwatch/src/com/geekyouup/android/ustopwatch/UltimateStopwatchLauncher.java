bf80c86f30759b9b38acb4d878fad460c3dd6f28
richard.hyndman@gmail.com <richard.hyndman@gmail.com@31a94f91-a260-7e09-7896-cf7e99866384>
2011-06-24 15:59:26 +0000

    removed log line
    git-svn-id: http://android-ultimatestopwatch.googlecode.com/svn/trunk@44 31a94f91-a260-7e09-7896-cf7e99866384

--- stopwatch/src/com/geekyouup/android/ustopwatch/UltimateStopwatchLauncher.java
+++ stopwatch/src/com/geekyouup/android/ustopwatch/UltimateStopwatchLauncher.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.util.Log;
 
 public class UltimateStopwatchLauncher extends Activity {
 
@@ -17,7 +16,6 @@ public class UltimateStopwatchLauncher extends Activity {
 		Intent startActivityIntent = null;
 		if (IS_HONEYCOMB && isXLargeScreen())
 		{
-			Log.d("USW","Launching TABLET Version");
 			startActivityIntent = new Intent(this, UltimateStopwatchTabletActivity.class);
 		}
 		else
@@ -28,16 +26,10 @@ public class UltimateStopwatchLauncher extends Activity {
 	}
 
 	private boolean isXLargeScreen() {
-
-		Log.d("Stopwartch", "SCREEN LAYOUT: "
-				+ (getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) + ", is HC: "
-				+ IS_HONEYCOMB);
-
 		if ((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_XLARGE) {
 
 			return true;
 		}
-
 		return false;
 	}
 
